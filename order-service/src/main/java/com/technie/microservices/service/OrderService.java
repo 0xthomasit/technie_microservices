f461aff2d3d380071f6a9ae1309d8b62f7c2e45d
Thomas Ngo <tuan.ngo.yj@renesas.com>
2026-02-03 16:13:51 +0700

    2 - Add Order service with main funcions into the workspace

---
+++ order-service/src/main/java/com/technie/microservices/service/OrderService.java
@@ -0,0 +1,26 @@
+package com.technie.microservices.service;
+
+import com.technie.microservices.dto.OrderRequest;
+import com.technie.microservices.model.Order;
+import com.technie.microservices.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public void placeOrder(OrderRequest orderRequest) {
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+        order.setPrice(orderRequest.price().multiply(BigDecimal.valueOf(orderRequest.quantity())));
+        order.setSkuCode(orderRequest.skuCode());
+        order.setQuantity(orderRequest.quantity());
+        orderRepository.save(order);
+    }
+}
